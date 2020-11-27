d495496184ddf00fd8ebef8832f5899f34a3aa58
Michael Griffith <griffithcmichael@gmail.com>
2020-11-27 14:35:20 -0600

    comment line 11

--- Develop/server.js
+++ Develop/server.js
@@ -8,6 +8,6 @@ const PORT = 8080;
 app.use(exprees.urlencoded({ extend: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
-
+// app.listen one line function watch video 11.24 1 hr 10 minutes in
 app.listen(PORT, => (console.log("Listening PORT: " + PORT));
 
