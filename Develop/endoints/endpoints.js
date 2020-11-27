55edcd3cf73bf85ef5c9b3c48573354be69cd426
Michael Griffith <griffithcmichael@gmail.com>
2020-11-27 14:10:47 -0600

    api notes and notes id -from notes 11.24

--- Develop/endoints/endpoints.js
+++ Develop/endoints/endpoints.js
@@ -1,7 +1,17 @@
 const fs = require('fs');
 const path = require('path');
+var notes = JSON.parse(data)
 
 
 applicationCache.get("/api/notes", function(req, res){
-    res.json(notes);
+    res.JSON(notes);
+});
+
+app.post("/api/notes", function(req, res){
+    let newNotes = req.body;
+    notes.push(newNotes);
+    return console.log("You added new note: "+newNotes.title+"to your library.") 
+});
+app.get("/api/notes/:id", function(req, res){
+    res.JSON(notes[req.params.id]);
 });
