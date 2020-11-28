0a82252e8b219ea8a477f5db73679e6305bd9b7d
Michael Griffith <griffithcmichael@gmail.com>
2020-11-28 17:55:20 -0600

    added data to delete button

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -116,7 +116,7 @@ const renderNoteList = (notes) => {
 
     if (withDeleteButton) {
       const $delBtn = $(
-        "<i class='fas fa-trash-alt float-right text-danger delete-note'>"
+        "<i class='fas fa-trash-alt float-right text-danger delete-note data=id'>"
       );
       $li.append($delBtn);
     }
