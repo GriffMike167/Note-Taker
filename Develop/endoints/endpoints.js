b53c4bf7838b462adee757fe267232b12a0d940e
Michael Griffith <griffithcmichael@gmail.com>
2020-11-27 18:31:12 -0600

    punc and placement endpoints

--- Develop/endoints/endpoints.js
+++ Develop/endoints/endpoints.js
@@ -9,11 +9,11 @@ fs.readFile('db/db.json', 'utf8', function(err,data){
 
     var notes = JSON.parse(data)
 
-    app.get("notes/", function(req. res){
+    app.get("/notes", function(req, res){
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
     app.get('*', function(req, res){
-        res.sendFile(path.join(__dirname, "../piblic/index.html"));
+        res.sendFile(path.join(__dirname, "../public/index.html"));
     });
     app.get("/api/notes", function(req, res){
         res.JSON(notes);
@@ -21,13 +21,14 @@ fs.readFile('db/db.json', 'utf8', function(err,data){
     app.post("/api/notes", function(req, res){
         let newNotes = req.body;
         notes.push(newNotes);
+        updateDb();
         return console.log("You added new note: "+newNotes.title+"to your library.") 
     });
     app.get("/api/notes/:id", function(req, res){
         res.JSON(notes[req.params.id]);
     });
     app.delete("/api/notes/:id", function(req, res){
-        notes.filter(req.params.id, [newNotes])
-});
+        notes.filter(req.params.id, 1,)
+    });
 
 })
