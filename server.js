9c39c8faf8e1dac2ae2bec9732a9e5f97cc0661d
Michael Griffith <griffithcmichael@gmail.com>
2020-11-27 21:21:04 -0600

    links

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const fs = require("fs");
-const path = require("path");
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extend: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
 
-require('./endpoints/endpoints')(app);
+require('./endoints/endpoints')(app);
 // app.listen one line function watch video 11.24 1 hr 10 minutes in
 app.listen(PORT, function () {
     console.log("Listening PORT: " + PORT)
