391bddb503a33ca026c22ab8d17dae0842c58425
Michael Griffith <griffithcmichael@gmail.com>
2020-11-27 18:35:41 -0600

    convert to arrow functions

--- Develop/endoints/endpoints.js
+++ Develop/endoints/endpoints.js
@@ -9,26 +9,20 @@ fs.readFile('db/db.json', 'utf8', function(err,data){
 
     var notes = JSON.parse(data)
 
-    app.get("/notes", function(req, res){
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
-    });
-    app.get('*', function(req, res){
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
-    app.get("/api/notes", function(req, res){
-        res.JSON(notes);
-    });
+    app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "../public/notes.html")));
+    
+    app.get('*', (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
+
+    app.get("/api/notes", (req, res) => res.JSON(notes));
+
     app.post("/api/notes", function(req, res){
         let newNotes = req.body;
         notes.push(newNotes);
         updateDb();
         return console.log("You added new note: "+newNotes.title+"to your library.") 
     });
-    app.get("/api/notes/:id", function(req, res){
-        res.JSON(notes[req.params.id]);
-    });
-    app.delete("/api/notes/:id", function(req, res){
-        notes.filter(req.params.id, 1,)
-    });
+    app.get("/api/notes/:id", (req, res) => res.JSON(notes[req.params.id]));
+
+    app.delete("/api/notes/:id", (req, res) => notes.filter(req.params.id, 1));
 
 })
