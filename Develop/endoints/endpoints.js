132c781d4f75560994b1a165d151472648943d63
Michael Griffith <griffithcmichael@gmail.com>
2020-11-27 20:13:47 -0600

    updates

--- Develop/endoints/endpoints.js
+++ Develop/endoints/endpoints.js
@@ -1,34 +1,46 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.readFile('db/db.json', 'utf8', function(err,data){
-    if (err){
-        return console.log(err);
-    }
+
+module.exposrts = app => {
+
+    fs.readFile('db/db.json', 'utf8', function(err,data){
+        if (err){
+            return console.log(err);
+        }
 
 
-    var notes = JSON.parse(data)
+        var notes = JSON.parse(data)
 
-    
-    
-    app.get("/api/notes", (req, res) => res.JSON(notes));
+        
+        
+        app.get("/api/notes", (req, res) => res.JSON(notes));
 
-    app.post("/api/notes", function(req, res){
-        let newNotes = req.body;
-        notes.push(newNotes);
-        updateDb();
-        return console.log("You added new note: "+newNotes.title+"to your library.") 
-    });
-    app.get("/api/notes/:id", (req, res) => res.JSON(notes[req.params.id]));
-
-    app.delete("/api/notes/:id", function (req, res) {
-            notes.filter(req.params.id, 1);
+        app.post("/api/notes", function(req, res){
+            let newNotes = req.body;
+            notes.push(newNotes);
             updateDb();
-            console.log("You deleted note: "+newNotes.title+"from your library.")
+            return console.log("You added new note: "+newNotes.title+"to your library.") 
         });
-    app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "../public/notes.html")));
+        app.get("/api/notes/:id", (req, res) => res.JSON(notes[req.params.id]));
 
-    app.get('*', (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
+        app.delete("/api/notes/:id", function (req, res) {
+                notes.filter(req.params.id, 1);
+                updateDb();
+                console.log("You deleted note: "+newNotes.title+"from your library.")
+            });
+        app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "public/notes.html")));
+
+        app.get('*', (req, res) => res.sendFile(path.join(__dirname, "public/index.html")));
+
+        function updateDb() {
+            fs.writeFile("db/db.json", JSON.stringify(notes), err => {
+                if (err) throw err;
+                return true;
+
+            });
+        }
 
 
-})
+    });
+};
--- Develop/server.js
+++ Develop/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const { allowedNodeEnvironmentFlags } = require("process");
+
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 3001;
 
 app.use(exprees.urlencoded({ extend: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
+
+require('./endpoints/endpoints')(app);
 // app.listen one line function watch video 11.24 1 hr 10 minutes in
-app.listen(PORT, => (console.log("Listening PORT: " + PORT));
+app.listen(PORT, function () {
+    console.log("Listening PORT: " + PORT)
+});
 
