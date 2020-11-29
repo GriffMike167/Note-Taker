17734a151d89f66d484158142e9edf34296e8185
Michael Griffith <griffithcmichael@gmail.com>
2020-11-28 19:55:26 -0600

    trying anything possible

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -116,7 +116,7 @@ const renderNoteList = (notes) => {
 
     if (withDeleteButton) {
       const $delBtn = $(
-        "<i class='fas fa-trash-alt float-right text-danger delete-note' data-id="+""+">"
+        "<i class='fas fa-trash-alt float-right text-danger delete-note' data-id="+i+">"
       );
       $li.append($delBtn);
     }
