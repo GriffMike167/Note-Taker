bf6acef030e622a090519d769b3e7dfe1c19d6cd
Michael Griffith <griffithcmichael@gmail.com>
2020-12-04 17:03:42 -0600

    update

--- routes/routes.js
+++ routes/routes.js
@@ -26,38 +26,14 @@ module.exports = app => {
             return res.json(newNotes)
             
         });
-        app.delete("/api/notes/:id", function(req, res) {
-            var condition = "id = " + req.params.id;
-            console.log("condition", condition);
-            notes.delete(condition, function(result) {
-                console.log(result);
-                if (result.affectedRows === 0) {
-                    // If no rows were changed, then the ID must not exist, so 404
-                    console.log(result.affectedRows)
-                    return res.status(404).end();
-                } else {
-                    res.status(200).end();
-                }
-            });
-            updateDb();
-        });
-        app.get("/api/notes/:id", function (req, res) {
+        
+        app.delete("/api/notes/:id", function (req, res) {
             res.json(notes);
             
         
             });
 
-        // app.delete("/api/notes/:id", function (req, res) {
-        //         var id = $(this).data("id")
-        //         for (var i = 0; i < id.length; i++) {
-        //       if (chosen === id[i].routeName) {
-        //         return res.json(id[i]);
-        //       }
-        //     };
-                
-        //         // console.log("You deleted note: "+newNotes.title+"from your library.")
-        //         // return res.json(notes)
-        //     });
+        
         app.get("/notes", function (req, res) {
                 return res.sendFile(path.join(__dirname, "../public/notes.html"));
             });
