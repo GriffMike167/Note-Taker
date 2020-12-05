5d3711b007e2913fb5bb642ebbd865df42be5397
Michael Griffith <griffithcmichael@gmail.com>
2020-12-05 14:53:37 -0600

    tutor changes again

--- routes/routes.js
+++ routes/routes.js
@@ -1,49 +1,31 @@
 const fs = require('fs');
-const path = require('path');
+const path = require("path");
+const store = require('../db/store');
+const router = require("express").Router();
 
 
-module.exports = app => {
-
-    fs.readFile('db/db.json', 'utf8', (err,data) => {
-        if (err) throw err;
-            
-        
-
-
-        var notes = JSON.parse(data);
 
         
-        app.post("/api/notes", function(req, res){
-            let newNotes = req.body;
-            notes.push(newNotes);
-            updateDb();
-            console.log("You added new note: "+newNotes.title+" to your library.") 
-            return res.json(newNotes)
-            
+       router.get("/notes", (rew, res) => {
+           store 
+            .getNotes()
+            .then((notes)=> res.join(notes))
+            .catch((err)=> res.status(500).json(err));
+       });
+           
+       router.post("/notes", (req, res) => {
+           store
+            .addBody(req.body)
+            .then((note)=> res.json(notes))
+            .catch((err)=> res.status(500).json(err));
+       })
+        
+        router.delete("/notes/:id", (req, res) => {
+            store
+            .removeNote(req.params.id)
+            .then(() => res.json({ ok: true}))
+            .catch((err) => res.status(500).json(err));
         });
-            app.get("/api/notes/:id", function(req, res) {
-            // What does this code do?
-            var chosen = req.params.id;
-            console.log(chosen);
-          
-            // What does this code do?
-            for (var i = 0; i < notes.length; i++) {
-              if (chosen === notes[i].routeName) {
-                return res.json(notes[i]);
-              }
-            }
-          
-            // What does this code do?
-            return res.send("No character found");
-          });
-
-       
-        
-        app.delete("/:id", function (req, res) {
-            res.json(notes);
-            
-        
-            });
 
         
         app.get("/notes", function (req, res) {
@@ -54,14 +36,4 @@ module.exports = app => {
                 return res.sendFile(path.join(__dirname, "../public/index.html"));
             });
 
-        function updateDb() {
-            fs.writeFile("db/db.json", JSON.stringify(notes, '\t'), err => {
-                if (err) throw err;
-                return true;
-
-            });
-        };
-
-
-    });
-};
+        
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
-const fs = require("fs");
-const path = require('path');
+const routes = require("./routes/routes.js")
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,7 +8,8 @@ app.use(express.urlencoded({ extend: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
 
-require('./routes/routes')(app);
+app.use("/api", routes);
+
 // app.listen one line function watch video 11.24 1 hr 10 minutes in
 app.listen(PORT, function () {
     console.log("Listening PORT: " + PORT)
