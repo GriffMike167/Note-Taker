7d0618b04aba81cf7e27e3f6032cd2efeb5479af
Michael Griffith <griffithcmichael@gmail.com>
2020-12-05 14:58:45 -0600

    chnages

--- routes/routes.js
+++ routes/routes.js
@@ -29,11 +29,11 @@ const router = require("express").Router();
 
         
         app.get("/notes", function (req, res) {
-                return res.sendFile(path.join(__dirname, "../public/notes.html"));
+                res.sendFile(path.join(__dirname, "../public/notes.html"));
             });
 
         app.get('*', function (req, res) {
-                return res.sendFile(path.join(__dirname, "../public/index.html"));
+                res.sendFile(path.join(__dirname, "../public/index.html"));
             });
 
         
