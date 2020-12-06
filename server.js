0d8e2c4fffa9e5b4cc753fff113b204d224a6b7b
Michael Griffith <griffithcmichael@gmail.com>
2020-12-05 19:58:39 -0600

    upDates

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const fs = require("fs");
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extend: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
 
 require('./routes/routes')(app);
-// app.listen one line function watch video 11.24 1 hr 10 minutes in
-app.listen(PORT, function () {
-    console.log("Listening PORT: " + PORT)
-});
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});  
 
